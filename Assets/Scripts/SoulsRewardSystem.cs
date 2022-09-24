615be0610ce4be7b5de1423857de2334cf134388
Adhan Razzaque <adhan.razzaque@gmail.com>
2022-09-24 18:19:37 -0500

    Fill out starter code for souls reward system

--- Assets/Scripts/SoulsRewardSystem.cs
+++ Assets/Scripts/SoulsRewardSystem.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class SoulsRewardSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static SoulsRewardSystem Instance { get; private set; }
+    
+    void Awake()
     {
+        // Makes this a singleton instance
+        if (Instance != null && Instance != this)  // Instance is populated and is not already this
+        {
+            Destroy(this);
+        }
+        else  // Instance is not populated or is already this
+        {
+            Instance = this;
+        }
         
+        // Set me to Don't Destroy on Load when I start
+        DontDestroyOnLoad(this);
     }
 
     // Calculate the reward given the parameters 
     int CalculateReward(int height, int impactForce, int duration)
     {
+        // TODO: Ben fill out a reward calculation system
         return 0;
     }
 }
