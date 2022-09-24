eb1d83468f20975fbd601cc472756b9d500c428e
Adhan Razzaque <adhan.razzaque@gmail.com>
2022-09-24 18:09:37 -0500

    Add Reward System Script

---
+++ Assets/Scripts/SoulsRewardSystem.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoulsRewardSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Calculate the reward given the parameters 
+    int CalculateReward(int height, int impactForce, int duration)
+    {
+        return 0;
+    }
+}
