d26b123b809827e9afc9b2b6d89f1e135547c5d4
Adhan Razzaque <adhan.razzaque@gmail.com>
2022-09-25 19:27:53 -0500

    Add missing meta

---
+++ Assets/Scripts/End.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class End : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioManager.Instance.Play("HeyGuysImHome");  // 7 seconds
+        Invoke(nameof(PlayYesGod), 7f); // 2 seconds
+        Invoke(nameof(PlayWatchTheDogs), 9f); // 6.5 seconds
+        Invoke(nameof(PlayWasHeNot), 15.5f); // 4 seconds
+    }
+    
+    public void PlayYesGod()
+    {
+        AudioManager.Instance.Play("YesGod");
+    }
+    
+    public void PlayWatchTheDogs()
+    {
+        AudioManager.Instance.Play("WatchTheDogs");
+    }
+
+    public void PlayWasHeNot()
+    {
+        AudioManager.Instance.Play("WasHeNot");
+    }
+}
